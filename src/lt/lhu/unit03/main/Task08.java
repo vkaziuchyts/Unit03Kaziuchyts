93ff08c7037b5b92378f54dcf634177e9a735623
vkaziuchyts <kwn33vic@gmail.com>
2023-02-12 13:23:21 +0300

    Fix formatting

--- src/lt/lhu/unit03/main/Task08.java
+++ src/lt/lhu/unit03/main/Task08.java
@@ -3,25 +3,23 @@ package lt.lhu.unit03.main;
 public class Task08 {
 
 	public static void main(String[] args) {
-		
-		  double a = 0; 
-	      double b = 10;
-	      double h = 1; 
-	      double y;
-	      	      
-	      System.out.println(" -----------------------");
-	      System.out.println("|     x     |     y     |");
+		double a = 0;
+		double b = 10;
+		double h = 1;
+		double y;
 
-	      for (double x = a; x <= b; x += h) {
-	            y = 2 * (Math.tan(x / 2)) + 1;
-	            
-	      System.out.println(" -----------------------");
-	      System.out.printf("|%-11f|%-11f|\n", x , y);
-	      }
+		System.out.println(" -----------------------");
+		System.out.println("|     x     |     y     |");
 
-	      System.out.println(" -----------------------");
-	      
+		for (double x = a; x <= b; x += h) {
+			y = 2 * (Math.tan(x / 2)) + 1;
+
+			System.out.println(" -----------------------");
+			System.out.printf("|%-11f|%-11f|\n", x, y);
+		}
+
+		System.out.println(" -----------------------");
+
 	}
-	     
-}
 
+}
--- src/lt/lhu/unit03/main/Task09.java
+++ src/lt/lhu/unit03/main/Task09.java
@@ -3,19 +3,17 @@ package lt.lhu.unit03.main;
 public class Task09 {
 
 	public static void main(String[] args) {
-		
 		int x;
 		double fx;
-		
+
 		x = 9;
-		
-		if (x <= - 3) {
+
+		if (x <= -3) {
 			fx = 9;
-			
 		} else {
 			fx = 1 / (Math.pow(x, 2) + 1);
-			
-		System.out.println("F(x) = " + fx);
+
+			System.out.println("F(x) = " + fx);
 		}
 
 	}
